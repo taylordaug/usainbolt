ead9a4a000f7cd4266838599e22361df6e854fed
Taylor Daugherty <taylor.l.daugherty@disney.com>
2017-12-12 11:37:28 -0800

    init git repo

---
+++ usainbolt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  usainbolt
+//
+//  Created by Daugherty, Taylor L. on 12/11/17.
+//  Copyright © 2017 Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
